c7af30da0e32f3c541cfef02ffa96207fbb99cfb
Devin Crowley <devin.g.crowley@gmail.com>
2019-05-06 11:23:51 -0400

    Add folder: ardent, files: transform.py & __init__.py

---
+++ ardent/transform.py
@@ -0,0 +1,13 @@
+# transform is the primary class in ARDENT.
+
+# import statements.
+
+class transform():
+    """transform stores the deformation that is output by a registration \
+    and provides methods for applying that transformation to various images."""
+    
+    def __init__(self):
+        return
+    
+
+    
