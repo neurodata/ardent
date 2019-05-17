e285432e4c4cd94c57ec38e2c1a87598ea47d6a3
Devin Crowley <devin.g.crowley@gmail.com>
2019-05-17 13:08:23 -0400

    Add file: ardent/__version__.py, defines package metadata

---
+++ ardent/__version__.py
@@ -0,0 +1,25 @@
+r"""
+             _____    _____    ______   _   _   _______ 
+     /\     |  __ \  |  __ \  |  ____| | \ | | |__   __|
+    /  \    | |__) | | |  | | | |__    |  \| |    | |   
+   / /\ \   |  _  /  | |  | | |  __|   | . ` |    | |   
+  / ____ \  | | \ \  | |__| | | |____  | |\  |    | |   
+ /_/    \_\ |_|  \_\ |_____/  |______| |_| \_|    |_|   
+                                                        
+
+"""
+
+__title__ = "ardent"
+__description__ = "A tool for image registration."
+__url__ = "https://github.com/neurodata/ardent"
+__version__ = "0.0.1"
+__author__ = "Devin Crowley"
+__author_email__ = "devin.g.crowley@gmail.com"
+__license__ = "Eclipse Public License 2.0"
+__copyright__ = "Copyright 2019 Devin Crowley"
+__python_requires__ = ">=3.6"
+__required_packages__ = [
+    'numpy',
+    'pathlib',
+    'pytorch',
+]
