cf38ebacb9725629660183bfb24d0684e0a85313
Devin Crowley <devin.g.crowley@gmail.com>
2019-05-17 13:09:30 -0400

    Update file: setup.py, refactor to pull metadata from ardent/__version__.py

--- setup.py
+++ setup.py
@@ -2,48 +2,34 @@ import sys
 from setuptools import setup, find_packages
 from pathlib import Path
 
-PACKAGE_NAME = "ardent"
-DESCRIPTION = "A tool for image registration."
-with open("README.md", "r") as readmeFile:
-    LONG_DESCRIPTION = readmeFile.read()
-AUTHOR = "Devin Crowley"
-AUTHOR_EMAIL = "devin.g.crowley@gmail.com"
-URL = "https://github.com/neurodata/ardent"
-LICENSE = "Eclipse Public License 2.0"
-MINIMUM_PYTHON_VERSION = 3, 6  # Minimum of Python 3.6
-REQUIRED_PACKAGES = [
-    "numpy",
-    "pathlib",
-    "pytorch",
-]
+# Variables defined in __version__.py.
+version_info = {}
+with open(Path(__file__) / '__version__.py', 'r') as versionFile:
+    exec(versionFile.read(), version_info)
 
-# Find ardent version.
-PROJECT_PATH = str(Path(__file__).parent)
-for line in open(Path(PROJECT_PATH) / "ardent" / "__init__.py"):
-    if line.startswith("__version__ = "):
-    VERSION = line.strip().split()[2][1:-1] ##################################################### INVESTIGATE
-
+# Text contained in README.md.
+with open('README.md', 'r') as readmeFile:
+    README = readmeFile.read()
 
 def check_python_version():
     """Exit when the Python version is too low."""
     if sys.version_info < MINIMUM_PYTHON_VERSION:
         raise SystemExit(f"Python {MINIMUM_PYTHON_VERSION[0]}.{MINIMUM_PYTHON_VERSION[1] is required. \
             You are running Python {sys.version_info[0]}.{sys.version_info[1]}.")
-
-
 check_python_version()
 
 setup(
-    name=PACKAGE_NAME,
-    version=VERSION,
-    description=DESCRIPTION,
-    long_description=LONG_DESCRIPTION,
-    long_description_content_type="text/markdown",
-    author=AUTHOR,
-    author_email=AUTHOR_EMAIL,
-    install_requires=REQUIRED_PACKAGES,
-    url=URL,
-    license=LICENSE,
+    name=version_info['__title__'],
+    description=version_info['__description__'],
+    long_description=README,
+    long_description_content_type='text/markdown',
+    version=version_info['__version__'],
+    url=version_info['__url__'],
+    author=version_info['__author__'],
+    author_email=version_info['__author_email__'],
+    python_requires=version_info['__python_requires__'],
+    install_requires=version_info['__required_packages__'],
+    license=version_info['__license__'],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Science/Research",
