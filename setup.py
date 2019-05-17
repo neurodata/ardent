8e1cf8409482a86f59abbbca48abe598e5e3223e
Devin Crowley <devin.g.crowley@gmail.com>
2019-05-17 16:14:22 -0400

    Update file: setup.py, update check_python_version and python_requires to use __min_python_version__ instead of __python_requires__

--- setup.py
+++ setup.py
@@ -13,8 +13,8 @@ with open('README.md', 'r') as readmeFile:
 
 def check_python_version():
     """Exit when the Python version is too low."""
-    if sys.version_info < MINIMUM_PYTHON_VERSION:
-        raise SystemExit((f"Python {MINIMUM_PYTHON_VERSION[0]}.{MINIMUM_PYTHON_VERSION[1]} is required."
+    if sys.version_info < version_info['__min_python_version__']:
+        raise SystemExit((f"Python {version_info['__min_python_version__'][0]}.{version_info['__min_python_version__'][1]} is required."
             f"\nYou are running Python {sys.version_info[0]}.{sys.version_info[1]}."))
 check_python_version()
 
@@ -27,7 +27,7 @@ setup(
     url=version_info['__url__'],
     author=version_info['__author__'],
     author_email=version_info['__author_email__'],
-    python_requires=version_info['__python_requires__'],
+    python_requires=f">={version_info['__min_python_version__'][0]}.{version_info['__min_python_version__'][1]}",
     install_requires=version_info['__required_packages__'],
     license=version_info['__license__'],
     classifiers=[
