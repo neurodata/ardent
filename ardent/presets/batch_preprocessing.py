a58c2a10c5e5271b1ff33a7516b39ddb6bbf263f
Devin Crowley <devin.g.crowley@gmail.com>
2019-06-03 18:01:37 -0400

    Update file: ardent/presets/batch_preprocessing.py, add precise description to basic_preprocessing docstring

--- ardent/presets/batch_preprocessing.py
+++ ardent/presets/batch_preprocessing.py
@@ -1,7 +1,10 @@
 from ardent.preprocessing import preprocess
 
 def basic_preprocessing(data):
-    """Call each of the listed preprocessing functions on data and return the result."""
+    """Call each of the listed preprocessing functions on data and return the result.
+    - normalize_by_MAD
+    - center_to_mean
+    - pad"""
 
     return preprocess(data, ['normalize_by_MAD', 'center_to_mean', 'pad'])
 
