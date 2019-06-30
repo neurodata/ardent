8b8cd648c222689116e669d0ff608f6d6d4bb58a
Devin Crowley <devin.g.crowley@gmail.com>
2019-06-30 13:18:09 -0400

    Update files: ardent/transform.py & demo/demo.ipynb, disable xI and xJ in register and partially clean up demo file

--- ardent/transform.py
+++ ardent/transform.py
@@ -63,7 +63,7 @@ class Transform():
         # TODO: clean up this hotfix.
         xI = [np.arange(nxyz_i)*dxyz_i - np.mean(np.arange(nxyz_i)*dxyz_i) for nxyz_i, dxyz_i in zip(template.shape, template_resolution)]
         xJ = [np.arange(nxyz_i)*dxyz_i - np.mean(np.arange(nxyz_i)*dxyz_i) for nxyz_i, dxyz_i in zip(target.shape, target_resolution)]
-        registration_parameters.update(xI=xI, xJ=xJ)
+        # registration_parameters.update(xI=xI, xJ=xJ)
 
         outdict = torch_register(template, target, **registration_parameters)
         '''outdict contains:
