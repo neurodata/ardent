de0d0af615816a0616dba05ae203b47299f9ea36
Devin Crowley <devin.g.crowley@gmail.com>
2019-09-10 11:54:52 -0400

    Comment out imports from .presets in ardent/__init__.py for testing

--- ardent/__init__.py
+++ ardent/__init__.py
@@ -10,6 +10,6 @@ from . import preprocessing # Subpackage.
 from .preprocessing import preprocess # Function.
 
 from . import presets # Subpackage.
-from .presets import basic_preprocessing # Function.
-from .presets import get_registration_presets # Function.
-from .presets import get_registration_preset # Function.
+# from .presets import basic_preprocessing # Function.
+# from .presets import get_registration_presets # Function.
+# from .presets import get_registration_preset # Function.
