a9193d8f836c7ce0f770954106f4062a2f509383
Devin Crowley <devin.g.crowley@gmail.com>
2019-09-10 11:17:21 -0400

    Update docs/conf.py to pull the version from ardent/__version__.py

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath(".."))
 # -- Project information -----------------------------------------------------
 
 project = "ARDENT"
-copyright = "2019, Devin Crowley"
+copyright = "2019"
 author = "Devin Crowley"
 
 # The full version, including alpha/beta/rc tags
